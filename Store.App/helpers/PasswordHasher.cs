f7e3b0f6e64cc822648dc271d3fd701757618a12
ZahraaEmarah <zahraa.emara@syngenta.com>
2023-12-09 11:31:34 +0200

    using md5

---
+++ Store.App/helpers/PasswordHasher.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store.App.helpers
+{
+    public class PasswordHasher
+    {
+        public string HashPassword(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            byte[] data = System.Text.Encoding.ASCII.GetBytes(salt + password);
+            data = System.Security.Cryptography.MD5.Create().ComputeHash(data);
+            return Convert.ToBase64String(data);
+        }
+    }
+}
