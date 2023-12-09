6a1ec2ce9d9a5294c6d5e990eaa8437f36714609
ZahraaEmarah <zahraa.emara@syngenta.com>
2023-12-09 10:51:47 +0200

    Register operation

--- Store.App/Register.Designer.cs
+++ Store.App/Register.Designer.cs
@@ -28,12 +28,98 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.userNameTextBox = new System.Windows.Forms.TextBox();
+            this.emailTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.confirmPasswordTextBox = new System.Windows.Forms.TextBox();
+            this.registerBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Georgia", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(291, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(197, 41);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "New User";
+            // 
+            // userNameTextBox
+            // 
+            this.userNameTextBox.Location = new System.Drawing.Point(244, 108);
+            this.userNameTextBox.Name = "userNameTextBox";
+            this.userNameTextBox.PlaceholderText = "Username";
+            this.userNameTextBox.Size = new System.Drawing.Size(310, 31);
+            this.userNameTextBox.TabIndex = 1;
+            this.userNameTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // emailTextBox
+            // 
+            this.emailTextBox.Location = new System.Drawing.Point(244, 164);
+            this.emailTextBox.Name = "emailTextBox";
+            this.emailTextBox.PlaceholderText = "Email";
+            this.emailTextBox.Size = new System.Drawing.Size(310, 31);
+            this.emailTextBox.TabIndex = 2;
+            this.emailTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.Location = new System.Drawing.Point(244, 221);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.PasswordChar = '*';
+            this.passwordTextBox.PlaceholderText = "Password";
+            this.passwordTextBox.Size = new System.Drawing.Size(310, 31);
+            this.passwordTextBox.TabIndex = 3;
+            this.passwordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // confirmPasswordTextBox
+            // 
+            this.confirmPasswordTextBox.Location = new System.Drawing.Point(244, 279);
+            this.confirmPasswordTextBox.Name = "confirmPasswordTextBox";
+            this.confirmPasswordTextBox.PasswordChar = '*';
+            this.confirmPasswordTextBox.PlaceholderText = "Confirm Password";
+            this.confirmPasswordTextBox.Size = new System.Drawing.Size(310, 31);
+            this.confirmPasswordTextBox.TabIndex = 4;
+            this.confirmPasswordTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // registerBtn
+            // 
+            this.registerBtn.Location = new System.Drawing.Point(342, 349);
+            this.registerBtn.Name = "registerBtn";
+            this.registerBtn.Size = new System.Drawing.Size(112, 34);
+            this.registerBtn.TabIndex = 5;
+            this.registerBtn.Text = "Register";
+            this.registerBtn.UseVisualStyleBackColor = true;
+            this.registerBtn.Click += new System.EventHandler(this.registerBtn_Click);
+            // 
+            // Register
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(800, 426);
+            this.Controls.Add(this.registerBtn);
+            this.Controls.Add(this.confirmPasswordTextBox);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.emailTextBox);
+            this.Controls.Add(this.userNameTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Register";
             this.Text = "Register";
+            this.Load += new System.EventHandler(this.Register_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Label label1;
+        private TextBox userNameTextBox;
+        private TextBox emailTextBox;
+        private TextBox passwordTextBox;
+        private TextBox confirmPasswordTextBox;
+        private Button registerBtn;
     }
 }
--- Store.App/Register.cs
+++ Store.App/Register.cs
@@ -1,9 +1,14 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Cryptography.KeyDerivation;
+using Store.Data;
+using Store.Domain;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.Entity;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,9 +17,56 @@ namespace Store.App
 {
     public partial class Register : Form
     {
+        StoreContext context;
         public Register()
         {
+            context = new StoreContext();
             InitializeComponent();
+        }
+
+        private void Register_Load(object sender, EventArgs e)
+        {
+            context.User.Load();
+        }
+
+        private bool Validate_Password(string password, string confirm_password)
+        {
+            return !password.Equals("") && !confirm_password.Equals("") && confirm_password.Equals(password);
+        }
+
+        private string Hash_Password(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(128 / 8);
+            string hashed = Convert.ToBase64String(KeyDerivation.Pbkdf2(
+            password: password!,
+            salt: salt,
+            prf: KeyDerivationPrf.HMACSHA256,
+            iterationCount: 100000,
+            numBytesRequested: 256 / 8));
+
+            return hashed;
+        }
+
+        private void registerBtn_Click(object sender, EventArgs e)
+        {
+            if (Validate_Password(passwordTextBox.Text, confirmPasswordTextBox.Text))
+            {
+                User NewUser = new()
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = userNameTextBox.Text,
+                    Email = emailTextBox.Text,
+                    Password = Hash_Password(passwordTextBox.Text),
+                };
+                context.Add(NewUser);
+                context.SaveChanges();
+                MessageBox.Show("Registeration Success");
+                this.Hide();
+            }
+            else
+            {
+                MessageBox.Show("Invalid Password");
+            }
         }
     }
 }
